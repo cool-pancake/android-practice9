99aaf57c36d3360ec7b0629733757a9772540213
neo <whocares>
2024-11-10 15:49:58 +0900

    fix calc

--- app/src/main/java/com/example/android_practice9/MainActivity.kt
+++ app/src/main/java/com/example/android_practice9/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android_practice9
 
 import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -16,14 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
 
         binding.buttonDel.setOnClickListener {
             binding.input.text = " "
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.button0.setOnClickListener {
+            Log.d("TestTag", "asdsassss")
             addToInputText("0")
         }
 
